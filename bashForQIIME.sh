fccaec21ada6829fdfdc45fc66c35ce150e2d9fa
Joshua Wells <joshwells637@gmail.com>
2024-06-05 12:39:25 -0400

    Testing File Iteration

--- bashForQIIME.sh
+++ bashForQIIME.sh
@@ -5,34 +5,15 @@
 
 echo -e "Welcome to Bash for QIIME."
 
-echo -e "Please enter a name for the folder for this session: "
-read dirname
-mkdir $dirname
+echo -e "Please ensure that all sample files have been placed into the QIIME Input folder on the desktop: "
+sampleloc = /mnt/c/users/mjbea/onedrive/desktop/QIIME_Input
 
-echo -e "Please enter the name of your metadata file (CASE SENSITIVE): "
-read metadata
+for file in "$sampleloc"/*; do
+  if [ -f "$file" ]; then
+    echo "$file"
+  fi
+done
 
-# Will need to change this for more general use, moving pictures tutorial used as template
-mkdir emp-single-end-sequences
-echo -e "Enter the name of your barcodes file: "
-read barcodes
-echo -e "Enter the name of your sequences file: "
-read seqs
-echo -e "Please enter a name to use for the demultiplexed sequence file (include the file type as ".qza"): "
-read demux
-echo -e "Please enter a name to use for the demultiplexed sequence details file (include the file type as ".qza"): "
-read demux_details
-echo -e "Please enter a name to use for the demultiplexed sequence visualization (include the file type as a ".qzv"): "
-read demux_vis
+# Need to prompt user for sample file names so that they may be renamed to the .fastq format
 
-qiime tools import --type EMPSingleEndSequences --input-path emp-single-end-sequences --output-path emp-single-emd-sequences.qza
-
-
-qiime demux emp-single --i-seqs emp-single-end-sequences.qza --m-barcodes-file $metadata --m-barcodes-column barcode-sequence ooo-per-sample-sequences $demux --o-error-correction-details $demux_details
-
-qiime demux summarize --i-data $demux --o-visualization $demux_vis
-cp $demux_vis /mnt/c/users/mjbea/onedrive/desktop
-
-echo -e "The demultiplexed sequence visualization has been copied to your desktop for ease of viewing."
-echo -e "After viewing this visualization, how many bases wo
-
+#gzip *.fastq
